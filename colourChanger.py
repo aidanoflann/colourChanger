ab33ea4a48631a81b75b648b4d35a4d8f8f6a641
Aidan <pi@raspberrypi.(none)>
2015-11-15 18:12:47 +0000

    request and change commands working from remote test python script

--- colourChanger.py
+++ colourChanger.py
@@ -1,18 +1,7 @@
 from flask import Flask, request
+from urlparse import parse_qs
 application = Flask(__name__)
 
-#couple of simple URL request tests
-#/ will be replaced with a page showing the current colour
-@application.route("/")
-def hello():
-	print "Some guy or gal just accessed /."
-	return "<center><div style='width:auto; padding:auto'><h1 style='color:purple'>Hello There!</h1></div></center>"
-
-#example of another page
-@application.route("/test")
-def test():
-	print "Some bloke just accessed /test."
-	return "<h1 style='color:yellow'>Testing...</h1>"
 
 #function to change the colour saved in currentColour.txt
 def changeColour(colour):
@@ -21,17 +10,46 @@ def changeColour(colour):
 		colourFile.write(colour)
 	colourFile.close()
 
+#function to request the colour saved in currentColour.txt
+def requestColour():
+	colourFile = open("currentColour.txt", "r")
+	colour = colourFile.read()
+	colourFile.close()
+	return colour
+
+#couple of simple URL request tests
+#/ will be replaced with a page showing the current colour
+@application.route("/")
+def hello():
+	print "Some guy or gal just accessed /."
+	#determine the currentColour
+	colour = requestColour()
+	return "<center><div style='background-colour:" + colour + "width:auto; padding:auto'><h1 style='color:" + colour + "'>Hello There!</h1></div></center>"
+
+#example of another page
+@application.route("/test")
+def test():
+	print "Some bloke just accessed /test."
+	return "<h1 style='color:yellow'>Testing...</h1>"
+
 #if a post request is received:
 @application.route("/", methods = ['POST'])
 def changeColourPost():
-	if request.method == 'POST':
-		print "post request received"
-		print request.get_data()
-		changeColour('fffeef')
-		return "Colour has been changed."
+	if request.method == "POST":
+		print "post request received."
+		request_data = parse_qs(request.get_data())
+		print "data parsed."
+		if request_data['type'][0] == "change":
+			print "change request received"
+			changeColour(request_data['colour'][0])
+			return "Colour has been changed."
+		elif request_data['type'][0] == "request":
+			print "request request received"
+			return requestColour()
+		else:
+			return "Unrecognised request type."
 	else:
-		return "Colour has not been changed."
-
+		return "Not a POST request"
 
 if __name__ == "__main__":
     application.run()
